397f3531a176158100d58654c3cf3299f3517a09
Evhen Polyakov <xxx.qqq1990@gmail.com>
2026-05-31 15:28:17 +0200

    fixed error homework

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -11,7 +11,7 @@ export const getNoteById = async (req, res)=> {
     const note = await Note.findById(noteId);
 
     if(!note) {
-        throw createHttpError(404, "Student not found");
+        throw createHttpError(404, "Note not found");
     }
     res.status(200).json(note);
 }; 
@@ -28,7 +28,7 @@ export const deleteNote = async (req, res,) => {
     });
 
     if(!note){
-        throw createHttpError(404, "Student not found");
+        throw createHttpError(404, "Note not found");
     }
 
     res.status(200).json(note);
@@ -39,7 +39,7 @@ export const updateNote = async(req, res)=> {
     const note = await Note.findOneAndUpdate({_id: noteId}, req.body,{returnDocument: "after"},);
 
     if(!note) {
-        throw createHttpError(404, "Student not found");
+        throw createHttpError(404, "Note not found");
     }
     res.status(200).json(note);
 };
