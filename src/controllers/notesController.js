19a60a5a15ee6d66774d6f317ceb380b9c6a4572
Evhen Polyakov <xxx.qqq1990@gmail.com>
2026-06-05 18:28:23 +0200

    homework-3

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,40 @@ import createHttpError from "http-errors";
 import { Note } from "../models/note.js";
 
 export const getAllNotes = async (req,res) => {
-    const notes = await Note.find();
-    res.status(200).json(notes);
+
+    const {page = 1, perPage = 10, tag, search} = req.query;
+    const skip = (page - 1) * perPage;
+
+    const myQuery =  Note.find();
+
+    if(tag){
+        myQuery.where("tag").equals(tag);
+    }
+
+    if(search){
+        myQuery.where({
+            $or:[
+                {title: {$regex: search,$options: 'i'}},
+                { content: {$regex: search, $options: 'i'}},
+            ],
+        });
+    }
+
+    const [totalNotes, notes] = await Promise.all([
+        myQuery.clone().countDocuments(),
+        myQuery.skip(skip).limit(perPage),
+    ]);
+
+    const totalPages = Math.ceil(totalNotes / perPage);
+
+    res.status(200).json({
+        
+        page,
+        perPage, 
+        totalNotes, 
+        totalPages, 
+        notes,
+        });
 };
 
 export const getNoteById = async (req, res)=> {
--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import { TAGS } from "../constants/tags.js";
 
 const noteSchema = new Schema(
     {
@@ -14,8 +15,7 @@ const noteSchema = new Schema(
         },
         tag:{
             type: String,
-            enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
-            default: 'Todo',
+            enum: TAGS,
         },
     },
     {
