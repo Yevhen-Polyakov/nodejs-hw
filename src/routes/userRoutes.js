01e315e7c9cc2e460a21fda389f48f4fbb98160b
Evhen Polyakov <xxx.qqq1990@gmail.com>
2026-06-20 21:43:47 +0200

    fixed error homework

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { authenticate } from "../middleware/authenticate";
-import { upload } from "../middleware/multer";
-import { updateUserAvatar } from "../controllers/userController";
+import { authenticate } from "../middleware/authenticate.js";
+import { upload } from "../middleware/multer.js";
+import { updateUserAvatar } from "../controllers/userController.js";
 
 const router = Router();
 
